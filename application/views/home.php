f427e2a9216ce5161957c4269b4e01e062f05311
Silva Soluções Tech <silvasolucoes2023@gmail.com>
2023-04-05 19:40:44 -0300

    Atualizações

--- application/views/home.php
+++ application/views/home.php
@@ -1,18 +1,36 @@
 <?php include_once 'header.php'; ?>
 
 <main>
-    <section class="banner text-center">
+    <section class="banner">
         <!-- o conteúdo do banner vai aqui -->
         <div class="banner-overlay">
-            <div class="banner-text text-center">
-                <h2 class="banner-title">MAIS QUE ATLETAS</h2>
-                <h2 class="banner-subtitle">FORMAMOS CIDADÃOS</h2>
-                <p class="banner-cta">MATRICULE SEU FILHO!</p>
+            <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active"></div>
+                    <div class="container">
+                        <div class="carousel-caption text-start">
+                            <h1>MAIS QUE ATLETAS</h1>
+                            <p>FORMAMOS CIDADÃOS</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
     </section>
     <section class="latest-news">
       <!-- o conteúdo das notícias mais recentes vai aqui -->
+      
     </section>
     <section class="corpo-diretivo">
       <!-- o conteúdo do corpo diretivo vai aqui -->
