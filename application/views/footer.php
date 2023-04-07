153f495ea553e5826184acc5390f6380e2f6df41
José Windenberg Andrade de Melo <windenbergandrade@gmail.com>
2023-04-07 13:27:44 -0300

    Update footer
    O footer está quase completo

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,61 @@
-  <footer>
-    <!-- footer content goes here -->
-  </footer>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Header com Bootstrap 5</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <style>
+  .bi {
+    font-size: 2.5em;
+  }
+  </style>
+</head>
+<body>
+<footer class="text-white p-5" style="background-color: #003680;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <img src="../../public/images/logo-02.png" class="img-fluid" style="width: 214px;height: auto;">
+      </div>
+      <div class="col-md-4">
+        <h5>Privacidade</h5>
+        <p>Politica de Privacidade</p>
+        <div class="col-sm-6 col-md-3">
+        <h5 class="mb-3 text-nowrap">Redes Sociais</h5>
+        <div class="d-flex align-items-center">
+          <div class="me-3">
+            <i class="bi bi-facebook"></i>
+          </div>
+          <div class="me-3">
+            <i class="bi bi-instagram"></i>
+          </div>
+          <div class="me-3">
+            <i class="bi bi-whatsapp"></i>
+          </div>
+          <div class="me-3">
+            <i class="bi bi-envelope"></i>
+          </div>
+        </div>
+      </div>
+
+      </div>
+      <div class="col-md-3">
+        <h5>Tem alguma pergunta?</h5>
+        <p>Rua Riacho Madeira, s.n. Centro , Lajes - RN,59535-000
+        +55 (84)99620-2499</p>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-md-12 text-center">
+        <small class="text-white">Copyright © 2023 Todos os direitos reservados | Desenvolvido por Silva Soluções Tech</small>
+      </div>
+    </div>
+  </div>
+</footer>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js" integrity="sha512-tfYBk2s8b4hZPTx9T9TID11RKPyM8WgHwGWKpDqwA+Yg0RAsWELG1xHbc7e6a0q6IZQRf6wM6yDnNNy93U/0Rw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-9/u46rZndQr/U8vgw2uQ5NhJGpYq3GRT5zbR/JxkG9X+nv8k6RBAaJG2JNv6zkmw6nr1z6KjEbHjRO/n8n9D/Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
