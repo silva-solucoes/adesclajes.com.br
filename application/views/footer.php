d7fdcfe45dfcff44783878a53d2371269415d647
Silva Soluções Tech <123086804+silva-solucoes@users.noreply.github.com>
2023-04-10 13:18:51 -0300

    Correção do menu suspenso FUTEBOL

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Header com Bootstrap 5</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    <style>
+<style>
   .bi {
     font-size: 2.5em;
   }
-  </style>
-</head>
-<body>
+</style>
+
 <footer class="text-white p-5" style="background-color: #003680;">
   <div class="container">
     <div class="row">
@@ -25,40 +14,42 @@
         <h5>Privacidade</h5>
         <p>Politica de Privacidade</p>
         <div class="col-sm-6 col-md-3">
-        <h5 class="mb-3 text-nowrap">Redes Sociais</h5>
-        <div class="d-flex align-items-center">
-          <div class="me-3">
-            <i class="bi bi-facebook"></i>
-          </div>
-          <div class="me-3">
-            <i class="bi bi-instagram"></i>
-          </div>
-          <div class="me-3">
-            <i class="bi bi-whatsapp"></i>
-          </div>
-          <div class="me-3">
-            <i class="bi bi-envelope"></i>
+          <h5 class="mb-3 text-nowrap">Redes Sociais</h5>
+          <div class="d-flex align-items-center">
+            <div class="me-3">
+              <i class="bi bi-facebook"></i>
+            </div>
+            <div class="me-3">
+              <i class="bi bi-instagram"></i>
+            </div>
+            <div class="me-3">
+              <i class="bi bi-whatsapp"></i>
+            </div>
+            <div class="me-3">
+              <i class="bi bi-envelope"></i>
+            </div>
           </div>
         </div>
-      </div>
 
       </div>
       <div class="col-md-3">
         <h5>Tem alguma pergunta?</h5>
         <p>Rua Riacho Madeira, s.n. Centro , Lajes - RN,59535-000
-        +55 (84)99620-2499</p>
+          +55 (84)99620-2499</p>
       </div>
     </div>
     <div class="row mt-3">
       <div class="col-md-12 text-center">
-        <small class="text-white">Copyright © 2023 Todos os direitos reservados | Desenvolvido por Silva Soluções Tech</small>
+        <small class="text-white">Copyright © 2023 Todos os direitos reservados | Desenvolvido por Silva Soluções
+          Tech</small>
       </div>
     </div>
   </div>
 </footer>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js" integrity="sha512-tfYBk2s8b4hZPTx9T9TID11RKPyM8WgHwGWKpDqwA+Yg0RAsWELG1xHbc7e6a0q6IZQRf6wM6yDnNNy93U/0Rw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-9/u46rZndQr/U8vgw2uQ5NhJGpYq3GRT5zbR/JxkG9X+nv8k6RBAaJG2JNv6zkmw6nr1z6KjEbHjRO/n8n9D/Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="../../public/js/script.js"></script>
+
+<script src="../../public/js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
-<html lang="pt-BR" data-bs-theme="auto">
+<!doctype html>
+<html lang="pt-BR">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ADESC Lajes</title>
     <link rel="icon" type="image/png" href="../../public/images/favicon.png">
     <link rel="stylesheet" href="../../public/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-x0yHE7q5YH62lV5ucdJFs9XVpKuMcIajR1RyKywGyLd7yvK+LSKpyCZBt/G9XtO2OClzrBwxjI1GyVNgN5Y5EA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="../../public/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../../public/css/bootstrap-icons.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap');
 
@@ -25,34 +25,39 @@
             height: auto;
         }
 
-        .collapse button{
+        .collapse button {
             margin-left: 40px;
         }
+
         @media (min-width: 991px) {
-        .img-redu {
-            display: none;
-        }
+            .img-redu {
+                display: none;
+            }
         }
 
         @media (max-width: 767px) {
-        .img-redu {
-            display: block;
-        }
+            .img-redu {
+                display: block;
+            }
         }
     </style>
 </head>
 
 <body>
+
     <!-- BOTÃO DE ROLAR A PÁGINA AO TOPO -->
     <a href="#" id="botao-topo" class="btn-topo mostrar"><i class="fas fa-chevron-up"></i></a>
+
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-            <div class=" justify-content-center align-items-center container-fluid d-flex flex-column flex-sm-row" id="navbarNav">
-                    <a class="navbar-brand img-redu" href="#" style="margin: 0px;">
-                        <img class="navbar-img" src="../../public/images/logo-01.png" alt="Logo">
-                    </a>
-                
-                <div class="collapse navbar-collapse justify-content-center align-items-center" id="navbarTogglerDemo03">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class=" justify-content-center align-items-center container-fluid d-flex flex-column flex-sm-row"
+                id="navbarNav">
+                <a class="navbar-brand img-redu" href="#" style="margin: 0px;">
+                    <img class="navbar-img" src="../../public/images/logo-01.png" alt="Logo">
+                </a>
+
+                <div class="collapse navbar-collapse justify-content-center align-items-center"
+                    id="navbarTogglerDemo03">
                     <a class="navbar-brand" href="#" style="margin: 0px;">
                         <img class="navbar-img" src="../../public/images/logo-01.png" alt="Logo">
                     </a>
@@ -69,14 +74,15 @@
                             <a class="nav-link" href="#">Notícias</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
                                 Futebol
                             </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">SUB 09</a>
-                                <a class="dropdown-item" href="#">SUB 13</a>
-                                <a class="dropdown-item" href="#">SUB 15</a>
-                            </div>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">SUB 09</a></li>
+                                <li><a class="dropdown-item" href="#">SUB 13</a></li>
+                                <li><a class="dropdown-item" href="#">SUB 15</a></li>
+                            </ul>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Sobre</a>
@@ -87,13 +93,25 @@
                     </ul>
                     <button type="button" class="btn btn-warning rounded-pill text-white">Area de inscrição</button>
                 </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-toggle="modal" data-bs-target="#meuModal">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-toggle="modal"
+                    data-bs-target="#meuModal">
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </div>
         </nav>
     </header>
 
-</body>
-
-</html>
+    <!-- MODAL DE INSCRIÇÃO -->
+    <div class="modal fade" id="meuModal" tabindex="-1" aria-labelledby="meuModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title w-bold mb-0 fs-2" id="meuModalLabel">Sign up for free</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Conteúdo do modal aqui...</p>
+                </div>
+            </div>
+        </div>
+    </div>
