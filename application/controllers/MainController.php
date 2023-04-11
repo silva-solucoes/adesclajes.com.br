637224fec93f1825e8a363540f8d3de84f1e141d
José Windenberg Andrade de Melo <windenbergandrade@gmail.com>
2023-04-10 22:36:45 -0300

    Erro - paginação
    Erro - paginação

---
+++ application/controllers/MainController.php
@@ -0,0 +1,9 @@
+<?php
+
+class MainController {
+    public function home() {
+        header('Location:http://localhost/adesclajes/application/views/header.php');
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('Controllers/MainController.php');
+
+$controller = new MainController();
+
+$controller->home();
+
+?>
