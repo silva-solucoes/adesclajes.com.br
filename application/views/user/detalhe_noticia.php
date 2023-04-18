53f54e213ae0b3432ae013fdb92ca54662bce7f8
Silva Soluções Tech <123086804+silva-solucoes@users.noreply.github.com>
2023-04-18 20:30:36 -0300

    Criação da lista de comentários e form de comentár

--- application/views/user/detalhe_noticia.php
+++ application/views/user/detalhe_noticia.php
@@ -9,9 +9,9 @@
             <ol>
                 <li><a href="<?php echo URL . 'index' ?>">Início</a></li>
                 <li><a href="<?php echo URL . 'noticias' ?>">Notícia</a></li>
-                <li>Título da Notícia</li>
+                <li>ADESC vence de virada e segue na liderança do campeonato de futebol local</li>
             </ol>
-            <h2>Título da Notícia</h2>
+            <h2>ADESC vence de virada e segue na liderança do campeonato de futebol local</h2>
 
         </div>
     </section><!-- End Breadcrumbs -->
@@ -48,7 +48,7 @@
                         <h3>Informações da Postagem</h3>
                         <ul>
                             <li><strong>Categória</strong>: Futsal</li>
-                            <li><strong>Local</strong>: Lajes/RN</li>
+                            <li><strong>Autor</strong>: ADESC Lajes</li>
                             <li><strong>Postado em</strong>: 18 Abril 2023</li>
                             <li><strong>Compartilhe</strong>: <a href="#">www.example.com</a></li>
                         </ul>
@@ -56,15 +56,17 @@
                     <div class="portfolio-info">
                         <h3>Últimas Notícias</h3>
                         <ul>
-                            <li><a href="#"><strong>Título da Notícia</strong>: Breve descrição</a></li>
-                            <li><a href="#"><strong>Título da Notícia</strong>: Breve descrição</a></li>
-                            <li><a href="#"><strong>Título da Notícia</strong>: Breve descrição</a></li>
-                            <li><a href="#"><strong>Título da Notícia</strong>: Breve descrição</a></li>
+                            <li><a href="#"><strong>O prefeito Felipe Menezes marcou presença no campeonato
+                                        local</strong></a></li>
+                            <li><a href="#"><strong>Entrega de materiais esportivos fortalece atletas da
+                                        ADESC</strong></a></li>
+                            <li><a href="#"><strong>Entrega de materiais esportivos promove mais qualidade e desempenho
+                                        para atletas da ADESC</strong></a></li>
                         </ul>
                     </div>
                 </div>
 
-                <div class="col-lg-12">
+                <div class="col-lg-8">
                     <div class="portfolio-description">
                         <h2>ADESC vence de virada e segue na liderança do campeonato de futebol local</h2>
                         <p>
@@ -77,30 +79,87 @@
                     </div>
                 </div>
 
-                <div class="col-lg-12">
+                <!--COMPARTILHAMENTO-->
+                <div class="col-lg-8">
+                    <div class="portfolio-info">
+                        <h3>Compartilhe</h3>
+                        <div class="compart-links mt-3">
+                            <a href="https://www.facebook.com/sharer/sharer.php?u=URL_DA_PÁGINA" class="facebook"
+                                target="_blank"><i class="bx bxl-facebook"></i></a>
+                            <a href="https://twitter.com/intent/tweet?url=SUA_URL_AQUI&text=SUA_MENSAGEM_AQUI"
+                                class="twitter" target="_blank"><i class="bx bxl-twitter"></i></a>
+                            <a href="instagram://share?url=[URL DA PÁGINA]" class="instagram"><i
+                                    class="bx bxl-instagram"></i></a>
+                            <a href="mailto:SEU_EMAIL_AQUI?subject=ASSUNTO_AQUI&body=MENSAGEM_AQUI"
+                                class="google-plus"><i class="bx bxl-google-plus"></i></a>
+                            <a href="whatsapp://send?text=Confira%20essa%20notícia:%20https://www.example.com/noticia"
+                                class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+                            <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://www.exemplo.com"
+                                class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <!--COMENTÁRIOS E FORMULÁRIO DE COMENTAR-->
+                <div class="col-lg-8">
                     <div class="portfolio-info">
                         <h3>Comentários</h3>
-                        <ul>
-                            <li>
-                                <!--
-                                <?php foreach ($comentarios as $comentario) { ?>
-                                    <p><strong>
-                                            <?php echo $comentario['nome']; ?>:
-                                        </strong>
-                                        <?php echo $comentario['comentario']; ?>
-                                    </p>
-                                <?php } ?> -->
-                            </li>
-                            <li>
-                                <div class="form-group">
-                                    <label for="name">Menssagem:</label>
-                                    <textarea class="form-control" name="message" rows="10" required></textarea>
+                        <div class="row g-4 py-2 mb-4 row-cols-1 row-cols-lg-3">
+                            <div class="feature col">
+                                <div
+                                    class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                                    <i class="bi bi-chat-left-dots"></i>
                                 </div>
-                            </li>
-                            <li>
-                                <div class="text-center"><button type="submit">Enviar Comentário</button></div>
-                            </li>
-                        </ul>
+                                <h3>Marcos Rodrigues</h3>
+                                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with
+                                    another sentence and probably just keep going until we run out of words.</p>
+                                <a href="#" class="icon-link">Terça-feira, 18 de Abr de 2023</a>
+                            </div>
+                            <div class="feature col">
+                                <div
+                                    class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                                    <i class="bi bi-chat-left-dots"></i>
+                                </div>
+                                <h3>Maria Antônia</h3>
+                                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with
+                                    another sentence and probably just keep going until we run out of words.</p>
+                                <a href="#" class="icon-link">Segunda-feira, 17 de Abr de 2023</a>
+                            </div>
+                            <div class="feature col">
+                                <div
+                                    class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                                    <i class="bi bi-chat-left-dots"></i>
+                                </div>
+                                <h3>Paulo Vitor</h3>
+                                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with
+                                    another sentence and probably just keep going until we run out of words.</p>
+                                <a href="#" class="icon-link">Domingo, 16 de Abr de 2023</a>
+                            </div>
+                        </div>
+                        <h3>Deixe um comentário</h3>
+                        <form action="forms/contact" method="post" role="form" class="php-form">
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    <label for="name">Seu Nome:</label>
+                                    <input type="text" name="name" class="form-control" id="name" required="">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="name">Seu E-mail:</label>
+                                    <input type="email" class="form-control" name="email" id="email" required="">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="name">Menssagem:</label>
+                                <textarea class="form-control" name="message" rows="10" required=""></textarea>
+                            </div>
+                            <div class="my-3">
+                                <div class="loading">Carregando</div>
+                                <div class="error-message"></div>
+                                <div class="sent-message">Seu comentário foi enviada. Obrigado!</div>
+                            </div>
+                            <div class="text-center"><button type="submit"><i class="bi bi-chat-left-dots"></i> Enviar
+                                    Comentário</button></div>
+                        </form>
                     </div>
                 </div>
 
