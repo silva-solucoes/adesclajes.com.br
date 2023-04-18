dc1236b47bc4b352774502d52654aa9e0a7b53c6
Silva Soluções Tech <123086804+silva-solucoes@users.noreply.github.com>
2023-04-18 13:47:57 -0300

    Criação da Página de Esportes

---
+++ application/views/user/detalhe_esporte.php
@@ -0,0 +1,32 @@
+<?php include_once 'header.php'; ?>
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container">
+
+            <ol>
+                <li><a href="<?php echo URL . 'index' ?>">Início</a></li>
+                <li><a href="<?php echo URL . 'noticias' ?>">Notícia</a></li>
+                <li>Título da Notícia</li>
+            </ol>
+            <h2>Título da Notícia</h2>
+
+        </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Seção de Detalhes de Esporte ======= -->
+    <section id="portfolio-details" class="portfolio-details">
+        <div class="container">
+
+            <div class="row gy-4">
+                <p>Detalhes de Esportes</p>
+            </div>
+
+        </div>
+    </section><!-- End Seção de Detalhes de Esporte -->
+
+</main><!-- End #main -->
+
+<?php include_once 'footer.php'; ?>
