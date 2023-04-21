1a445701f83a4c3d5d8b17bd6aadc953b8528c30
Silva Soluções Tech <123086804+silva-solucoes@users.noreply.github.com>
2023-04-21 10:42:33 -0300

    Atualizações do Modal de Inscrrição

--- application/views/user/header.php
+++ application/views/user/header.php
@@ -82,21 +82,218 @@
     </div>
   </header><!-- End Header -->
 
-  <!-- ======= Inserindo Modal AGENDA DE CONSULTORIA ======= -->
+  <!-- ======= Inserindo Modal PRÉ INSCRIÇÃO ======= -->
   <!-- Modal -->
   <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
     aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
       <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="staticBackdropLabel">Agendar Consultoria</h1>
+        <div class="modal-header"
+          style="background-image: url('<?php echo URL; ?>public/images/fundo.jpg'); background-size: cover; width: 100% relative; height: 100px; color: #D98E04;">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Área de Inscrição</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <p class="mb-5 bt-5 text-center">Não perca a chance de fazer parte da Escolinha Esportiva
+            ADESC!<br>Inscreva-se agora
+            mesmo preenchendo nosso formulário e garanta seu lugar na equipe de sucesso!</p>
+          <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+
+            <div class="col-lg-5">
+              <div class="about-img">
+                <img src="<?php echo URL; ?>public/images/campeao.png" class="img-fluid" alt="">
+              </div>
+            </div>
+
+            <div class="col-lg-7">
+              <h3 class="pt-0 pt-lg-5" id="tituloForm">Formulário de Pré-Inscrição</h3>
+
+              <!-- Tabs -->
+              <ul class="nav nav-pills mb-3">
+                <li><a class="nav-link active" id="guias" data-bs-toggle="pill" href="#tab1">Informações do Atleta</a>
+                </li>
+                <li><a class="nav-link" id="guias" data-bs-toggle="pill" href="#tab2">Informações de Filiação</a></li>
+                <li><a class="nav-link" id="guias" data-bs-toggle="pill" href="#tab3">Informações de Responsável</a>
+                </li>
+              </ul><!-- End Tabs -->
+
+              <form class="needs-validation borda-form php-inscricao-form" novalidate>
+                <!-- Tab Content -->
+                <div class="tab-content">
+                  <!-- Informações do Atleta -->
+                  <div class="tab-pane fade show active" id="tab1">
+
+                    <p class="fst-italic">Informações Pessoais</p>
+
+                    <div class="row">
+                      <div class="form-group col-md-7">
+                        <label for="name">Nome Completo:</label>
+                        <input type="text" name="name" class="form-control" placeholder="Nome do Atleta completo" id="name" required>
+                      </div>
+                      <div class="form-group col-md-5">
+                        <label for="name">Gêrero:</label>
+                        <select class="form-select" aria-label="Default select example" required>
+                          <option selected>Selecione o gênero</option>
+                          <option value="1">Feminino</option>
+                          <option value="2">Masculino</option>
+                        </select>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label for="name">CPF:</label>
+                        <input type="text" name="cpf" class="form-control" placeholder="Somente o número" id="name" required>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label for="name">RG:</label>
+                        <input type="text" name="rg" class="form-control" placeholder="Somente o número" id="name" required>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label for="name">Data de Nascimento:</label>
+                        <input type="date" name="dataNascimento" class="form-control" id="name" required>
+                      </div>
+                      <div class="form-group col-md-5">
+                        <label for="name">Estado Civil:</label>
+                        <select class="form-select" aria-label="Default select example" required>
+                          <option selected>Selecione o Estado Civil</option>
+                          <option value="1">Solteiro/a</option>
+                          <option value="2">Casado/a</option>
+                          <option value="3">Divorciado/a</option>
+                          <option value="4">Viúvo/a</option>
+                        </select>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label for="name">Número do NIS:</label>
+                        <input type="text" name="nis" class="form-control" placeholder="Somente o número" id="name" required>
+                      </div>
+                      <div class="form-group col-md-12">
+                        <label for="name">Anexo do Registro:</label>
+                        <input class="form-control" type="file" id="formFileMultiple" multiple required>
+                      </div>
+                    </div>
+
+                    <div class="row">
+                      <div class="form-group col-md-7">
+                        <label for="name">Nome da Escola:</label>
+                        <input type="text" name="nomeEscola" class="form-control" placeholder="Nome completo da Escola" id="name" required>
+                      </div>
+                      <div class="form-group col-md-5">
+                        <label for="name">Nível de Ensino:</label>
+                        <select class="form-select" aria-label="Default select example" required>
+                          <option selected>Selecione o nível</option>
+                          <option value="1">Ensino Fundamental I</option>
+                          <option value="2">Ensino Fundamental II</option>
+                          <option value="3">Ensino Médio</option>
+                          <option value="4">Ensino Superior</option>
+                        </select>
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="name">Série:</label>
+                        <input type="text" name="serie" class="form-control" placeholder="Informe a série" id="name" required>
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="name">Turno:</label>
+                        <input type="text" name="turno" class="form-control" placeholder="Informe o turno" id="name" required>
+                      </div>
+                      <div class="form-group col-md-7">
+                        <label for="name">Categoria Esportiva que deseja atuar:</label>
+                        <select class="form-select" aria-label="Default select example" required>
+                          <option selected>Selecione uma categoria</option>
+                          <option value="1">Futsal</option>
+                          <option value="2">Hidroginástica</option>
+                          <option value="3">Natação</option>
+                          <option value="4">Taekwondo</option>
+                          <option value="5">Vôlei</option>
+                        </select>
+                      </div>
+
+                    </div>
+
+                  </div><!-- End Tab 1 Content -->
+
+                  <div class="tab-pane fade show" id="tab2">
+
+                    <p class="fst-italic">Informações do Pai</p>
+
+                    <div class="row">
+                      <div class="form-group col-md-7">
+                        <label for="name">Nome Completo do Pai:</label>
+                        <input type="text" name="namePai" class="form-control" placeholder="Nome completo do Pai" id="name" required>
+                      </div>
+                      <div class="form-group col-md-5">
+                        <label for="name">Telefone do Pai:</label>
+                        <input type="text" name="telefonePai" class="form-control" placeholder="Somente o número" id="name" required>
+                      </div>
+
+                      <p class="fst-italic mt-4">Informações do Mãe</p>
+
+                      <div class="form-group col-md-7">
+                        <label for="name">Nome Completo do Mãe:</label>
+                        <input type="text" name="nameMae" class="form-control" placeholder="Nome completo da Mãe" id="name" required>
+                      </div>
+                      <div class="form-group col-md-5">
+                        <label for="name">Telefone do Mãe:</label>
+                        <input type="text" name="telefoneMae" class="form-control" placeholder="Somente o número" id="name" required>
+                      </div>
+                    </div>
+
+                  </div><!-- End Tab 2 Content -->
+
+                  <div class="tab-pane fade show" id="tab3">
+
+                    <p class="fst-italic">Informações do Responsável</p>
+
+                    <div class="row">
+                      <div class="form-group col-md-7">
+                        <label for="name">Nome Completo do Responsável:</label>
+                        <input type="text" name="nameResponsavel" class="form-control" placeholder="Nome completo do Responsável" id="name" required>
+                      </div>
+                      <div class="form-group col-md-5">
+                        <label for="name">Grau de Parentestco:</label>
+                        <select class="form-select" aria-label="Default select example" required>
+                          <option selected>Selecione um grau</option>
+                          <option value="1">Pai/Mãe</option>
+                          <option value="2">Tio/Tia</option>
+                          <option value="3">Avô/Avó</option>
+                          <option value="4">Irmão/Irmã</option>
+                        </select>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label for="name">CPF:</label>
+                        <input type="text" name="CPFResponsavel" placeholder="Somente o número" class="form-control" id="name" required>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label for="name">RG:</label>
+                        <input type="text" name="rgResponsavel" placeholder="Somente o número" class="form-control" id="name" required>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label for="name">Telefone:</label>
+                        <input type="text" name="telefoneResponsavel" placeholder="Somente o número" class="form-control" id="name" required>
+                      </div>
+                      <div class="form-group col-md-7">
+                        <label for="name">Logradouro:</label>
+                        <input type="text" name="logradouro" class="form-control" placeholder="Informe o nome da rua" id="name" required>
+                      </div>
+                      <div class="form-group col-md-5">
+                        <label for="name">Nº da casa:</label>
+                        <input type="text" name="nCasa" class="form-control" placeholder="Nº" id="name" required>
+                      </div>
+                      <div class="form-group col-md-5">
+                        <label for="name">Bairro:</label>
+                        <input type="text" name="bairro" class="form-control" placeholder="Informe o bairro" id="name" required>
+                      </div>
+                    </div>
+
+                  </div><!-- End Tab 3 Content -->
+
+                </div>
+
+
+            </div>
+
+          </div>
         </div>
         <div class="modal-footer">
           <button id="botao-inscricao" type="button" class="btn btn-primary">Agendar</button>
+          </form>
           <button id="botao-inscricao" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
         </div>
       </div>
@@ -110,7 +307,8 @@
     <div class="modal-dialog modal-xl modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="staticBackdropLabel"><i class="bi bi-shield-lock"></i> Política de Privacidade da Escolinha ADESC Lajes</h1>
+          <h1 class="modal-title fs-5" id="staticBackdropLabel"><i class="bi bi-shield-lock"></i> Política de
+            Privacidade da Escolinha ADESC Lajes</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
